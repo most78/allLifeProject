99761bc3c3aaaed05a838092963a4f4b32bf518e
Marcos Ostos <marcos.ostos@gmail.com>
2018-08-10 15:10:02 +0200

    add build folder and prepare pre and pro config files

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -6,9 +6,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = {
     entry: ['./src/main.js'],
     output: {
-        filename: 'app.min.js',
+        filename: '[name].min.js',
         path: path.resolve(__dirname, '..', 'dist'),
         publicPath: '/'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.ts?$/, 
+                exclude: /node_modules/, 
+                loader: "ts-loader"
+            }
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({ template: './src/index.html' })
